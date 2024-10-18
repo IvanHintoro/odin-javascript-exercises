9b96760e72b75e9b7822c46f82a3a46298003d07
IvanHintoro <ivanhintoro@gmail.com>
2024-10-18 12:12:56 +0700

    Solved 03_reveseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(words) {
+    let reversed = words.split("").reverse().join("");
+    return reversed;
 };
 
 // Do not edit below this line
