b809e29eae2c98a7089fe7691f20fa4812794484
IvanHintoro <ivanhintoro@gmail.com>
2024-10-18 13:29:46 +0700

    Solved 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArr, ...args) {
+    for (let i=0; i<args.length; i++){
+        for (let j=0; j<myArr.length; j++){
+            if (myArr[j]===args[i]){
+                myArr.splice(j,1);
+                j--;
+            }
+        }
+    }
+    return myArr;
 };
 
 // Do not edit below this line
