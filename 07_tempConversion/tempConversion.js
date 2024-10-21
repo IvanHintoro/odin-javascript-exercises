675ef2748fe2b8715c2c5d50142b6d8236d18be1
IvanHintoro <ivanhintoro@gmail.com>
2024-10-21 12:01:08 +0700

    Solved 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatures) {
+  let Celcius = ((temperatures - 32) * 5 ) / 9
+  if (Celcius % 1 != 0){
+    Celcius = parseFloat(Celcius.toFixed(1));
+  }
+  return Celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatures) {
+  let Fahrenheit = (temperatures * 9 / 5) + 32
+  if (Fahrenheit % 1 != 0){
+    Fahrenheit =  parseFloat(Fahrenheit.toFixed(1));
+  }
+  return Fahrenheit;
 };
 
 // Do not edit below this line
